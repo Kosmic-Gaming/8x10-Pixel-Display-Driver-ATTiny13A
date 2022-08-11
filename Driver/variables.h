b677da5e9f68a477c178b430810ade0ef1b5f9da
KosmicDaKerbal <88624986+Kosmic-Gaming@users.noreply.github.com>
2022-08-11 12:57:08 +0530

    Update variables.h

--- Driver/variables.h
+++ Driver/variables.h
@@ -8,14 +8,4 @@ byte arr7 = 0;  //00000000
 byte arr8 = 0;  //00000000
 byte arr9 = 0;  //00000000
 byte arr10 = 0; //00000000
-
-boolean b1;
-boolean b2;
-boolean b3;
-boolean b4;
-boolean b5;
-boolean b6;
-boolean b7;
-boolean b8;
-
 byte VRAMPointer;
